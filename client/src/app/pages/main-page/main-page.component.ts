2050008218cc35586b407747516c1de8880de8b9
Nikita Degtyarev <nikita.deki4@gmail.com>
2025-02-23 21:22:03 +0300

    готова главная страница

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   imports: [],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
 export class MainPageComponent {
+  router = inject(Router);
 
+  navigateToProducts = () => {
+    this.router.navigate(['/products']);
+  };
 }
