1fa97a7099d9ce0fb80f2133672e19df4b0dc385
Nikita Degtyarev <nikita.deki4@gmail.com>
2025-03-12 10:52:59 +0300

    done not found page

--- client/src/app/pages/not-found/not-found.component.ts
+++ client/src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
+  router = inject(Router);
 
+  moveBack() {
+    this.router.navigate(['']);
+  }
 }
