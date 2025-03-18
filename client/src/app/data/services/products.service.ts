360758c3a8d91deb5e954b0969f6d069008ff322
Nikita Degtyarev <nikita.deki4@gmail.com>
2025-03-18 12:28:35 +0300

    commit

--- client/src/app/data/services/products.service.ts
+++ client/src/app/data/services/products.service.ts
@@ -53,7 +53,7 @@ export class ProductsService {
         take(1),
         catchError(() =>
           this.#http
-            .get<ProductInterface[]>(`https://vkusmeda.shop/api/product${id}`)
+            .get<ProductInterface[]>(`https://vkusmeda.shop/api/product/${id}`)
             .pipe(take(1))
         )
       );
