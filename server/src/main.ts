84ad7b686566632780ed19212d94855696d89c99
Nikita Degtyarev <nikita.deki4@gmail.com>
2025-03-31 09:49:35 +0300

    commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,9 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      '*',
       'http://localhost:4200',
-      'http://185.240.48.56',
       'http://vkusmeda.shop',
       'https://vkusmeda.shop',
     ],
